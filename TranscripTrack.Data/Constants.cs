b3f6d3170b4ba42e406c6881afe35e7ba96016b7
Frank Harvey <frankharvey88@gmail.com>
2020-07-19 18:34:27 -0400

    renamed model to reflect more generalized use; added Constants.cs (initially to hold names of months)

---
+++ TranscripTrack.Data/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TranscripTrack.Data
+{
+    public static class Constants
+    {
+        public static readonly string[] Months = new string[]
+        {
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        };
+    }
+}
---
+++ TranscripTrack.Data/Models/LineRateEntryTotalModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TranscripTrack.Data.Models
+{
+    public class LineRateEntryTotalModel : BaseModel
+    {
+        public int? LineRateId { get; set; }
+        public string LineRate { get; set; }
+
+        public int TotalLines { get; set; }
+        public decimal TotalPay { get; set; }
+    }
+}
